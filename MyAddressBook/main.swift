95190cd9d03025428e9c4c33d381c46a24db2621
kouta <coffee_joe@hotmail.co.jp>
2015-11-06 21:16:00 +0900

    Initial Commit

---
+++ MyAddressBook/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MyAddressBook
+//
+//  Created by kouta on 2015/11/06.
+//  Copyright © 2015年 kouta. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
